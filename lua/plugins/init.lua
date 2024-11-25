0108352ac09f348315279006ad123323d2ac527a
gecarval <gersonrcarvalho//@gmail.com>
2024-11-25 20:09:56 +0100

    added base config

---
+++ lua/plugins/init.lua
@@ -0,0 +1,64 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "smoka7/hop.nvim",
+    opts = {
+      multi_windows = true,
+      keys = "htnsueoaidgcrlypmbkjvx",
+      uppercase_labels = true,
+    },
+    keys = {
+      {
+        "<leader>fj",
+        function()
+          require("hop").hint_words()
+        end,
+        mode = { "n", "x", "o" },
+        desc = "quick hop to a word",
+      },
+    },
+  },
+
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()  -- Mapping tab is already used by NvChad
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+    -- The mapping is set to other key, see custom/lua/mappings
+    -- or run <leader>cc to see copilot mapping section
+      vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end,
+    keys = {
+      {
+        "<leader>cc",
+        ":Copilot panel<CR>",
+        desc = "Copilot panel toggle",
+      },
+    },
+  },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
